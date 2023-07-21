cb74fd4b0f857f5709e026f2e0fcf33debe97fa8
ayadeleke <https://github.com/ayadeleke>
2023-07-20 18:36:04 -0700

    100-puppet_ssh_config.pp

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
 # sets up a client SSH configuration file so that we can connect to a server without using a password
-file { 'Name file identity':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  replace => true,
-}
-
-file { 'password authentication':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
+file { '/etc/ssh/ssh_config':
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
 }
