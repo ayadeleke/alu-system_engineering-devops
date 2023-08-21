db12cc41d73c9d6f452e063cd7ad63b5bda2cf81
ayadeleke <a.adeleke@alustudent.com>
2023-08-21 07:50:54 -0700

    0-the_sky_is_the_limit_not.pp

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# 'Increases the amount of traffic an Nginx server can handle.'
+# 'by increasing the ULIMIT in the default file'
+
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+}
